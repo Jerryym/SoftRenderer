19ca2dcd0e1a41659a28e0fb9fb02bad8976acb8
Jerry_yang <1622595386@qq.com>
2022-12-16 22:32:38 +0800

    完善对话框

--- Application/Dialog_Application.cpp
+++ Application/Dialog_Application.cpp
@@ -4,7 +4,26 @@ Dialog_Application::Dialog_Application(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    canvas = new QImage();
 }
 
 Dialog_Application::~Dialog_Application()
 {}
+
+void Dialog_Application::paintEvent(QPaintEvent * event)
+{
+    if (canvas)
+    {
+        //¥¥Ω®± À¢painter
+        QPainter painter(this);
+        painter.drawImage(0, 0, *canvas);
+    }
+    QWidget::paintEvent(event);
+}
+
+void Dialog_Application::receiveFrame(unsigned char* image)
+{
+    if (canvas)      delete canvas;
+    canvas = new QImage(image, width(), height(), QImage::Format_RGBA8888);
+    update();
+}
--- Application/Dialog_Application.h
+++ Application/Dialog_Application.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QPainter>
 #include "ui_Dialog_Application.h"
 
 class Dialog_Application : public QMainWindow
@@ -11,6 +12,11 @@ public:
     Dialog_Application(QWidget *parent = nullptr);
     ~Dialog_Application();
 
+protected:
+    void paintEvent(QPaintEvent* event) override;
+    void receiveFrame(unsigned char* image);
+
 private:
     Ui::Dialog_ApplicationClass ui;
+    QImage* canvas;                     //ªÊ÷∆∂‘œÛ
 };
